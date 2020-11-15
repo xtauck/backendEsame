c5b1fcf24f858fedc07379bba4a2d1685155df5b
Tauck <41118967+xtauck@users.noreply.github.com>
2020-11-15 21:47:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(cors())
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/t', (req, res) => {
   res.send('Hello Max!!!')
 })
