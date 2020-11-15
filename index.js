5bb88e8cf180c7b460a11651e5d281d1a0b9c210
xtauck <49095653+navirat@users.noreply.github.com>
2020-11-15 21:42:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+app.use(cors())
 app.get('/t', (req, res) => {
   res.send('Hello Liviu!')
 })
 
-app.listen(process.env.port || port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
