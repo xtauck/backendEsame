f823e6f5d94e9c757f09cabd46971779236e6df9
Tauck <41118967+xtauck@users.noreply.github.com>
2020-11-16 09:37:00 +0100

    bodyparser+post

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+app.use(bodyParser())
 
 app.get('/t', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
   res.send('Hello Max!!!')
 })
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.post('/calculate', (req, res) => {
+  console.log(req.body)
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.send(req.body)
 })
